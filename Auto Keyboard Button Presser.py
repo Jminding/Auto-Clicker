07ace10f454c1e0975715a41a248c8a5c4a0f6da
Jminding <69938575+Jminding@users.noreply.github.com>
2021-03-21 20:54:27 -0400

    Add files via upload

---
+++ Auto Keyboard Button Presser.py
@@ -0,0 +1,58 @@
+from pynput.keyboard import Key, Controller, Listener, KeyCode
+import time
+import pynput
+import threading
+
+delay = 0.001
+key = "3"
+start_stop_key = KeyCode(char='®')
+exit_key = KeyCode(char='≈')
+
+class PressKey(threading.Thread):
+
+    def __init__(self, delay, key):
+        super().__init__()
+        self.delay = delay
+        self.key = key
+        self.running = False
+        self.program_running = True
+
+    def start_pressing(self):
+        self.running = True
+
+    def stop_pressing(self):
+        self.running = False
+
+    def exit(self):
+        self.stop_pressing()
+        self.program_running = False
+
+    def run(self):
+        while self.program_running:
+            while self.running:
+                keyboard.press(self.key)
+                keyboard.release(self.key)
+                time.sleep(self.delay)
+
+    
+
+
+
+keyboard = Controller()
+press_thread = PressKey(delay, key)
+press_thread.start()
+
+
+def on_press(key):
+    if key == start_stop_key:
+        if press_thread.running:
+            press_thread.stop_pressing()
+        else:
+            press_thread.start_pressing()
+    elif key == exit_key:
+        press_thread.exit()
+        listener.stop()
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
+
